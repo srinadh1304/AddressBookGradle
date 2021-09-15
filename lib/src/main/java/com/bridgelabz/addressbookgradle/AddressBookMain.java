82bba40c86d8f08b6b1900107f4111983e483d0f
srinadh1304 <srinadh1304@gmail.com>
2021-09-15 17:19:34 +0530

    ADDED: create contacts in address book

---
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbookgradle;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to address book system");
+
+	}
+
+}
