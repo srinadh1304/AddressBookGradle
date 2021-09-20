471d5c920ae568196bdf2e7335f4385582f1d7b7
srinadh1304 <srinadh1304@gmail.com>
2021-09-20 21:00:56 +0530

    ADDED: Methods to perform IO operations

---
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookIo.java
@@ -0,0 +1,60 @@
+package com.bridgelabz.addressbookgradle;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookIo {
+
+	public static String ADDRESSBOOK_FILE_NAME = "AddressBook-file.txt";
+
+	public void writeData(List<Contact> contacts) {
+		
+		StringBuffer addressBuffer = new StringBuffer();
+		contacts.forEach(contact -> {
+			String addressDataString = contact.toString();
+			addressBuffer.append(addressDataString);
+		});
+
+		try {
+			Files.write(Paths.get(ADDRESSBOOK_FILE_NAME), addressBuffer.toString().getBytes());
+
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void printData() {
+		
+		try {
+			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath()).forEach(System.out::println);
+		}
+		catch(IOException e) {e.printStackTrace();}
+		
+	}
+
+	public List<String> readDataFromFile() {
+		
+		List<String> addressBookList = new ArrayList<String>();
+		
+		try {
+			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath())
+				.map(contact -> contact.trim())
+				.forEach(contact -> {
+					System.out.println(contact);
+					addressBookList.add(contact);
+			});
+			
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		return addressBookList;
+	}
+}
