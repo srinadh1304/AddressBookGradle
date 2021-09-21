da03341a9109bc1aab0770e645682a948570255e
srinadh1304 <srinadh1304@gmail.com>
2021-09-21 07:20:54 +0530

    MODIFIED: Method to read and write data to json file

--- lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookIo.java
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookIo.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.addressbookgradle;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
@@ -9,14 +13,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
+import com.google.gson.Gson;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import java.util.Arrays;
 
 public class AddressBookIo {
 
 	public static String ADDRESSBOOK_FILE_NAME = "AddressBook-file.txt";
 	public static String ADDRESSBOOK_CSV_FILE_NAME = "AddressBook-CSV.csv";
+	public static String ADDRESSBOOK_JSON_FILE_NAME = "AddressBook-JSON.json";
 	public void writeData(List<Contact> contacts) {
 
 		StringBuffer addressBuffer = new StringBuffer();
@@ -114,4 +120,41 @@ public class AddressBookIo {
 			e.printStackTrace();
 		}
 	}
+	public void writeDatatoJSON(List<Contact> addressBook) {
+		Gson gson = new Gson();
+		String json = gson.toJson(addressBook);
+		FileWriter writer;
+		try {
+			writer = new FileWriter(ADDRESSBOOK_JSON_FILE_NAME);
+			writer.write(json);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void readDataFromJSON() {
+		Gson gson = new Gson();
+		List<Contact> contactList = new ArrayList<Contact>();
+		BufferedReader bufferObject;
+		try {
+			bufferObject = new BufferedReader(new FileReader(ADDRESSBOOK_JSON_FILE_NAME));
+			Contact[] addressBookData = gson.fromJson(bufferObject, Contact[].class);
+			contactList = Arrays.asList(addressBookData);
+			for (Contact contact : contactList) {
+				System.out.println("First Name: "+contact.getFirstName());
+				System.out.println("Last Name: "+contact.getLastName());
+				System.out.println("City: "+contact.getCity());
+				System.out.println("State: "+contact.getState());
+				System.out.println("Zip: "+contact.getZip());
+				System.out.println("Phone number: "+contact.getPhoneNumber());
+				System.out.println("Email: "+contact.getEmail());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
 }
