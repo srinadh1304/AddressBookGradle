12eb21461bf21752f0c00b8db07073b9030d4075
srinadh1304 <srinadh1304@gmail.com>
2021-09-26 18:57:13 +0530

    ADDED: Interface with all method declarations

---
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookIF.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.addressbookgradle;
+
+import java.util.Scanner;
+
+import com.bridgelabz.addressbookgradle.AddressBookList.IOService;
+
+
+public interface AddressBookIF {
+	public void writeDataToFile(IOService ioService);
+	public void readDataFromFile();
+	public void writeDataToCsvFile();
+	public void readDataFromCsvFile();
+	public void findContactInCity(String cityName);
+	public void findContactInState(String stateName);
+	public void sortByName();
+	public void sortByCity();
+	public void sortByZip();
+	public void sortByState();
+	public void editContact();
+	public void deleteContact();
+	public void addContact(Contact contact);
+	public boolean checkIfContactExists(Contact contact);
+	public void printCountByCity();
+	public void printCountByState();
+	public void writeDataToJson() ;
+	public void readDataFromJson();
+	
+}
