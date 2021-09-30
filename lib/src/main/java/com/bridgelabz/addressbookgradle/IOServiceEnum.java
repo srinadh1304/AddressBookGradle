c7634134b7dcc34e45460dac76d2b2312d688c86
srinadh1304 <srinadh1304@gmail.com>
2021-09-30 21:44:16 +0530

    MODIFIED: added enum for database io

--- lib/src/main/java/com/bridgelabz/addressbookgradle/IOServiceEnum.java
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/IOServiceEnum.java
@@ -2,6 +2,6 @@ package com.bridgelabz.addressbookgradle;
 
 public class IOServiceEnum {
 	public enum IOService {
-		CONSOLE_IO, FILE_IO, CSV_IO,JSON_IO
+		CONSOLE_IO, FILE_IO, CSV_IO,JSON_IO,DB_IO
 	}
 }
