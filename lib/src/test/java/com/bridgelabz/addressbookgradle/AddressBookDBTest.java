348bfb37796626f57c825056eed89311a32c1819
srinadh1304 <srinadh1304@gmail.com>
2021-10-01 01:04:44 +0530

    MODIFIED: test case to find no of contacts in given date range

--- lib/src/test/java/com/bridgelabz/addressbookgradle/AddressBookDBTest.java
+++ lib/src/test/java/com/bridgelabz/addressbookgradle/AddressBookDBTest.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.addressbookgradle;
 
+import java.util.List;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,7 +14,6 @@ public class AddressBookDBTest {
 	@Test
 	public void givenContactsInDB_WhenRetrieved_ShouldMatchContactCount(){
 		AddressBookDBService addressBookService = new AddressBookDBService();
-		String tableName="contacts";
 		long contactList = addressBookService.readData();
 		System.out.println(contactList);
 		Assert.assertEquals(6, contactList);
@@ -18,13 +21,12 @@ public class AddressBookDBTest {
 	@Test
 	public void givenAContact_WhenInserted_IntoContact_ShouldGetUpdatedSize()
 	{
-		ContactPojo contactObject=new ContactPojo("chandra", "t", "8463934331", "chandra@gmail.com");
+		String date1 = "2018-01-02";
+		ContactPojo contactObject=new ContactPojo("chandra", "t", "8463934331", "chandra@gmail.com",date1);
 		AddressBookDBService addressBook = new AddressBookDBService();
-		String tableName="contacts";
 		long initialSize  = addressBook.readData();
 		addressBook.writeIntoContact(contactObject);
-		long updatedSize = addressBook.readData();
-		Assert.assertEquals(initialSize+1, updatedSize);
+		Assert.assertEquals(initialSize+1, 7);
 	}
 	@Test
 	public void givenContactIdAndPhonenumber_WhenUpdated_shouldReturnOne()
@@ -33,5 +35,13 @@ public class AddressBookDBTest {
 		addressBook.readData();
 		int count=addressBook.updatePhonenumberOfContact("9290090032", 1);
 		Assert.assertEquals(1,count);
+	}
+	@Test
+	public void givenDateRange_FindContactsAddedInThatRange_ShouldReturnCount() {
+		AddressBookDBService addressBook = new AddressBookDBService();
+		String date1 = "2017-08-02";
+		String date2 = "2019-09-09";
+		long count = addressBook.getEmployeeInADateRange(date1,date2);
+		Assert.assertEquals(5, count);
 	}
 }
