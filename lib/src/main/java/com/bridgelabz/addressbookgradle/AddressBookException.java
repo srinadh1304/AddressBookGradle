f88ee11c5c7cdeeb43119441dad44b05c5e7fd9e
srinadh1304 <srinadh1304@gmail.com>
2021-09-30 21:45:32 +0530

    MODIFIED: Added new exception type

--- lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookException.java
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookException.java
@@ -2,7 +2,7 @@ package com.bridgelabz.addressbookgradle;
 
 public class AddressBookException extends RuntimeException  {
 	enum ExceptionType {
-        CANNOT_EXECUTE_QUERY
+        CANNOT_EXECUTE_QUERY, FAILED_TO_CONNECT
     }
     ExceptionType exceptionType;
 
