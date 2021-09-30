bc6304b88d21cfcbf61cc24213ca705f9b67cb02
srinadh1304 <srinadh1304@gmail.com>
2021-09-30 21:44:39 +0530

    MODIFIED: added method to insert contact into database

--- lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookDBService.java
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookDBService.java
@@ -6,18 +6,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
 public class AddressBookDBService {
 	private PreparedStatement addressBookPreparedStatement;
 	private static AddressBookDBService addressBookDBService;
 	AddressBookDBService() {}
-	
+
 	public long readData() {
-		String sql = "select * from address_book;";
+		String sql = String.format("select * from contacts");
 		long count=0;
 		try(Connection connection = this.getConnection()) {
 			Statement statement = connection.createStatement();
 			ResultSet result = statement.executeQuery(sql);
-			
+
 			while(result.next()) {
 				String firstName = result.getString("firstName");
 				String lastName = result.getString("lastName");
@@ -25,18 +27,18 @@ public class AddressBookDBService {
 				String email = result.getString("email");
 				System.out.println(" First Name: "+firstName+" Last Name: "+lastName+"  Phone Number"+phoneNumber+" email"+email+" ");
 				count=count+1;
-				
+
 			}
 		} 
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return count;
-		
+
 	}
 
 	private Connection getConnection() throws SQLException {
-		String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service_old?useSSL=false";
+		String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service?useSSL=false";
 		String userName = "root";
 		String password = "Perfios@2021";
 		Connection connection;
@@ -45,7 +47,7 @@ public class AddressBookDBService {
 		System.out.println("connection is successful!"+connection);
 		return connection;
 	}
-	
+
 	public int getNumberOfContactsInACity(String city) {
 		int count = 0;
 		if(this.addressBookPreparedStatement == null) {
@@ -64,7 +66,7 @@ public class AddressBookDBService {
 		}
 		return count;
 	}
-	
+
 	private void prepareStatementForAddressBook() {
 		try {
 			Connection connection = this.getConnection();
@@ -75,6 +77,31 @@ public class AddressBookDBService {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void writeIntoContact(ContactPojo contact) {
+		Connection connection = null;
+		try {
+			connection = this.getConnection();
+		}
+		catch(Exception e) {
+			throw new AddressBookException(AddressBookException.ExceptionType.FAILED_TO_CONNECT, "Connection failed");
+		}
+
+		try(Statement statement = connection.createStatement()){
+			String sql = String.format("insert into contacts(firstName, lastName, phoneNumber, email) values ('%s','%s','%s','%s');", contact.getFirstName(),contact.getLastName()
+					,contact.getPhoneNumber(),contact.getEmail());
+			statement.execute(sql);
+		} 
+		catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			throw new AddressBookException(AddressBookException.ExceptionType.CANNOT_EXECUTE_QUERY, "Failed to execute query");
+		}
+
+	}
+
 
 }
