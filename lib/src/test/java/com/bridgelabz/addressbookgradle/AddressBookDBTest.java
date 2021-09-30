cb1878bff93b5be4efd412b86e5290dd7c8366e9
srinadh1304 <srinadh1304@gmail.com>
2021-09-30 21:46:21 +0530

    ADDED: Test case to validate insertion into contact table

--- lib/src/test/java/com/bridgelabz/addressbookgradle/AddressBookDBTest.java
+++ lib/src/test/java/com/bridgelabz/addressbookgradle/AddressBookDBTest.java
@@ -3,13 +3,27 @@ package com.bridgelabz.addressbookgradle;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bridgelabz.addressbookgradle.IOServiceEnum.IOService;
+
 
 public class AddressBookDBTest {
 	@Test
 	public void givenContactsInDB_WhenRetrieved_ShouldMatchContactCount(){
 		AddressBookDBService addressBookService = new AddressBookDBService();
+		String tableName="contacts";
 		long contactList = addressBookService.readData();
 		System.out.println(contactList);
-		Assert.assertEquals(8, contactList);
+		Assert.assertEquals(6, contactList);
+	}
+	@Test
+	public void givenAContact_WhenInserted_IntoContact_ShouldGetUpdatedSize()
+	{
+		ContactPojo contactObject=new ContactPojo("chandra", "t", "8463934331", "chandra@gmail.com");
+		AddressBookDBService addressBook = new AddressBookDBService();
+		String tableName="contacts";
+		long initialSize  = addressBook.readData();
+		addressBook.writeIntoContact(contactObject);
+		long updatedSize = addressBook.readData();
+		Assert.assertEquals(initialSize+1, updatedSize);
 	}
 }
