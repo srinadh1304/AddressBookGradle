8e98294d1855f5bd3b04b116a26e1a60c687434c
srinadh1304 <srinadh1304@gmail.com>
2021-09-30 20:40:53 +0530

    ADDED: Pojo class for address

---
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressPojo.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.addressbookgradle;
+
+public class AddressPojo {
+	
+	private int contact_id;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	
+	public AddressPojo(int contact_id, String address, String city, String state, int zip) {
+		super();
+		this.contact_id = contact_id;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	public int getContact_id() {
+		return contact_id;
+	}
+	public void setContact_id(int contact_id) {
+		this.contact_id = contact_id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	
+}
