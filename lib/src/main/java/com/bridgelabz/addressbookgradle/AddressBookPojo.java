a11df64a643858730817abd301eb556ae3e7792d
srinadh1304 <srinadh1304@gmail.com>
2021-09-30 20:41:39 +0530

    ADDED: Pojo class for address book

---
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookPojo.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbookgradle;
+
+public class AddressBookPojo {
+	private String addressBook_name;
+
+	public String getAddressBook_name() {
+		return addressBook_name;
+	}
+
+	public void setAddressBook_name(String addressBook_name) {
+		this.addressBook_name = addressBook_name;
+	}
+	
+}
