40cd02d65f792005ef1cccc4cfb55ecfb7e837cc
srinadh1304 <srinadh1304@gmail.com>
2021-10-05 07:38:12 +0530

    REFACTORED: removed id variable

--- lib/src/main/java/com/bridgelabz/addressbookgradle/Address.java
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/Address.java
@@ -2,26 +2,12 @@ package com.bridgelabz.addressbookgradle;
 
 public class Address {
 	
-	private int contact_id;
+	
 	private String address;
 	private String city;
 	private String state;
 	private int zip;
 	
-	public Address(int contact_id, String address, String city, String state, int zip) {
-		super();
-		this.contact_id = contact_id;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-	}
-	public int getContact_id() {
-		return contact_id;
-	}
-	public void setContact_id(int contact_id) {
-		this.contact_id = contact_id;
-	}
 	public String getAddress() {
 		return address;
 	}
