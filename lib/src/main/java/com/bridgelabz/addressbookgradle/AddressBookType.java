4d578b60f634b7a2a2d47134ffff61e281ebba14
srinadh1304 <srinadh1304@gmail.com>
2021-10-05 07:39:20 +0530

    REFACTORED: removed id variable

--- lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookType.java
+++ lib/src/main/java/com/bridgelabz/addressbookgradle/AddressBookType.java
@@ -3,13 +3,7 @@ package com.bridgelabz.addressbookgradle;
 public class AddressBookType {
 	private String addressBook_name;
 	private String addressBook_type;
-	private int contact_id;
-	public AddressBookType(String addressBook_name, String addressBook_type, int contact_id) {
-		super();
-		this.addressBook_name = addressBook_name;
-		this.addressBook_type = addressBook_type;
-		this.contact_id = contact_id;
-	}
+	
 	public String getAddressBook_name() {
 		return addressBook_name;
 	}
@@ -22,11 +16,6 @@ public class AddressBookType {
 	public void setAddressBook_type(String addressBook_type) {
 		this.addressBook_type = addressBook_type;
 	}
-	public int getContact_id() {
-		return contact_id;
-	}
-	public void setContact_id(int contact_id) {
-		this.contact_id = contact_id;
-	}
+	
 	
 }
